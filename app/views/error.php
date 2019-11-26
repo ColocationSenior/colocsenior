8ddf30f19d95d736a13efe68b85012dd505dcbb5
Halim AFIFI <afifihalim@hotmail.com>
2019-11-26 13:24:37 +0100

    Error page redesigned again

--- app/views/error.php
+++ app/views/error.php
@@ -1,47 +1,277 @@
 <?php
-$GLOBALS['view']['title'] = "Erreur 404";
+$GLOBALS['view']['title'] = "Erreur";
 include('includes/header.php') ?>
+
+<style>
+body {
+	background:whitesmoke;
+}
+
+.error-contain {
+	
+	height: 100%;
+	width: 100%;
+	margin: 200px auto 50px auto;
+	transform: translate(0%,0%);
+	background: #2E3C56;
+	border-radius: 10px;
+	box-shadow: 2px 2px 10px rgba(0,0,0,0.5);
+}
+.error-text h1 {
+    color: black;
+	font-size: 100px !important;
+	font-family: monospace;
+	margin: 0;
+	text-shadow: 1px 1px 1px #fff, -1px -1px 1px black;
+}
+.error-text {
+	text-align: center;
+	font-weight: bold;
+}
+.error-text p {
+	font-family: monospace;
+	font-size: 40px;
+    color: black;
+    
+}
+.error-text p:nth-child(3) {
+	margin-top: 150px;
+}
+.binoculars {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-60%, 20%);
+	transition: 1s;
+}
+.binoculars {
+/* 	animation-name: wiggle; */
+	animation-duration: 10s;
+	animation-delay: 5s;
+	animation-iteration-count: infinite;
+}
+
+.left-bino-lense, .right-bino-lense {
+	width: 45px;
+	height: 45px;
+	border-radius: 50%;
+	border: 5px solid black;
+	display: inline-block;
+	transform: skewX(-10deg);
+	background: white;
+	overflow: hidden;
+	position: relative;
+}
+.left-bino-lense {
+	margin-right: 10px;
+}
+.left-bino-lense::before, .right-bino-lense::before {
+	width: 20px;
+	height: 20px;
+	content: '';
+	display: block;
+	background: black;
+	position: absolute;
+	border: 3px solid #3066c9;
+	border-radius: 50%;
+	top: 50%;
+	left: 50%;
+	transform: translate(-70%,-55%);
+	animation-name: eye-move;
+	animation-duration: 6s;
+	animation-iteration-count: infinite;
+	animation-timing-function: ease-in-out;
+	animation-delay: 3s;
+}
+
+.back-bino {
+	width: 70px;
+	height: 30px;
+	background: black;
+	position: absolute;
+	top: 23px;
+	left: 40px;
+}
+.left-bino, .right-bino {
+	position: absolute;
+  height: 100px;
+  width: 80px;
+  perspective: 80px;
+	transform: rotate(100deg);
+	right: -25px;
+	top: 1px;
+}
+.right-bino {
+	left: -1px;
+}
+
+.left-bino::after, .right-bino::after {
+	content: '';
+	padding: 21px;
+	padding-top: 45px;
+  position: absolute;
+  border: 1px solid black;
+  background: black;
+  transform-style: preserve-3d;
+  transform: rotateX(30deg);
+	border-radius: 30% 80% 0 0;
+	left: 1px;
+	top: -4px;
+}
+
+.left-bino-lense .eye {
+	width: 100%;
+	height: 100%;
+}
+
+.eye::before, .eye::after {
+	content: '';
+	display: block;
+	width: 45px;
+	height: 26px;
+	background: #f5cd8c;
+	position: absolute;
+	transition: 1s;
+}
+.eye::before {
+	border-radius: 50% 50% 0 0;
+	border-bottom: 2px solid #b88c44;
+	top: -30px;
+	animation-name: eye-up;
+	animation-duration: 3s;
+	animation-iteration-count: infinite;
+}
+.eye::after {
+	border-radius: 0 0 50% 50%;
+	border-top: 2px solid #b88c44;
+	bottom: -30px;
+	animation-name: eye-down;
+	animation-duration: 3s;
+	animation-iteration-count: infinite;
+}
+
+.binoculars:hover .eye::after {
+	bottom: -30px;
+}
+.binoculars:hover .eye::before {
+	top: -30px;
+}
+
+@keyframes eye-up {
+	0% {
+		top: -30px;
+	}
+	50% {
+		top: -30px;
+	}
+	55% {
+		top: -5px;
+	}
+	60% {
+		top: -30px;
+	}
+}
+
+@keyframes eye-down {
+	0% {
+		bottom: -30px;
+	}
+	50% {
+		bottom: -30px;
+	}
+	55% {
+		bottom: -3px;
+	}
+	60% {
+		bottom: -30px;
+	}
+}
+
+@keyframes wiggle {
+	0% {
+		transform: translate(-50%,-20%);
+	}
+	10% {
+		transform: skew(-20deg) translate(-50%,-20%) rotate(20deg);
+	}
+	30% {
+		transform: skew(20deg) translate(-50%,-20%) rotate(-20deg);
+	}
+	40% {
+		transform: translate(-50%,-20%);
+	}
+	50% {
+		
+	}
+}
+
+@keyframes eye-move {
+	0% {
+		transform: translate(-70%,-55%);
+	}
+	10% {
+		transform: translate(-90%, -90%);
+	}
+	20% {
+		transform: translate(-60%, -110%);
+	}
+	30% {
+		transform: translate(-20%, -20%);
+	}
+	40% {
+		transform: translate(-60%, -0%);
+	}
+	50% {
+		transform: translate(-70%,-55%);
+	}
+}
+
+    
+</style>
+
+
 <div class="col g-ml-45 g-ml-0--lg g-pb-65--md">
-    <div class="g-pa-20">
-
-        <div class="media">
-            <div class=" justify-content-md-center">
-                <h1 class="g-font-weight-500 g-font-size-24 g-color-black mb-0">Une erreur est survenue ! </h1>
-            </div>
-        </div>
-        <hr class="d-flex g-brd-gray-light-v7 g-my-200">
-        <div class="row justify-content-lg-center g-my-200">
-
-            <div class="col-xl-3 col-md-6 g-mb-30 mt-xl-5">
-                <div class="card g-brd-gray-light-v7 rounded">
-                    <header !
-                        class="card-header g-bg-transparent g-brd-bottom-none g-pa-60 g-pa-30--sm justify-content-center">
-                        <h3 class="g-font-weight-900 g-font-size-20 g-color-black g-mb-15">  Cette page
-                    n'existe pas . . .</h3>
-                    </header>
-                    <hr class="d-flex g-brd-gray-light-v7 g-mx-0 g-mx-30--sm my-0 p-b-10">
-                    <div class="card-block g-px-20 g-px-30--sm g-py-15 g-py-20--sm"
-                        style="height:208px;overflow:hidden;background:url('/files/pictures/404.png') center center;background-size:cover;">
-                    </div>
-                </div>
-                <hr class="d-flex g-brd-gray-light-v7 g-my-20">
-                <div class="row justify-content-center">
-                    <a href="/"><span
-                            class="u-tags-v1 text-center g-width-200 g-brd-around g-bg-green g-color-white g-rounded-10 g-py-4 g-px-15"
-                            style="height:108px;overflow:hidden;background:url('/assets/img/logo/colocsenior_solo.png') center center;background-size:cover;"></span></a>
+        <div class="g-pa-20">
 
 
-                </div>
-            </div>
-        </div>
-
-
-
-
-
-
-
-    </div>
+        <div class="error-contain">
+	<div class="error-text">
+        <h1>404</h1>        
+		<p>Une erreur est survenue...</p>
+		<p>...car cette page n'existe pas.</p>
+	</div>
+	<div class="binoculars">
+		<div class="back-bino"></div>
+		<div class="left-bino"></div>
+		<div class="right-bino"></div>
+		<div class="left-bino-lense">
+			<div class="eye"></div>
+		</div>
+		<div class="right-bino-lense">
+			<div class="eye"></div>
+		</div>
+	</div>
 </div>
 
+        
+
+        </div>
+    </div>
+ 
+
+
+
+
 <?php include('includes/footer.php') ?>
+
+<script>
+    var card = $(".error-contain");
+
+    $(document).on("mousemove", function (e) {
+        var ax = -($(window).innerWidth() / 2 - e.pageX) / 25;
+        var ay = ($(window).innerHeight() / 2 - e.pageY) / 25;
+        card.attr("style", "transform: rotateY(" + ax + "deg) rotateX(" + ay +
+            "deg);-webkit-transform: rotateY(" + ax + "deg) rotateX(" + ay +
+            "deg);-moz-transform: rotateY(" + ax + "deg) rotateX(" + ay + "deg)");
+    });
+</script>
