9fab3e00d356ab9d86d0dab4fbe3d02dea6acb7f
Halim AFIFI <afifihalim@hotmail.com>
2019-12-12 17:43:42 +0100

    Some style and "responsive"

--- app/views/admin_contact.php
+++ app/views/admin_contact.php
@@ -6,16 +6,20 @@ include('includes/header.php') ?>
     <div class="g-pa-20">
         <div class="card">
             <div class="row no-gutters">
-                <div class="col-lg-5 w-100 g-brd-right--lg g-brd-gray-light-v7">
-                    <header class="g-brd-bottom g-brd-gray-light-v4 g-px-15 g-px-25--lg">
-                        <h3 class="g-font-weight-500 g-font-size-24 g-color-black justify-content-center"
-                            style="padding:25px 0 20px 0">
+                <div class="col-sm w-100 g-brd-right--lg g-brd-gray-light-v7">
+                    <header class="row d-inline-flex g-brd-bottom g-brd-gray-light-v4 g-px-15 g-px-25--lg">
+                    
+                        <h3 class="col-5 col-sm g-font-weight-500 g-font-size-24  text-success justify-content-center "
+                            style="padding:25px 0 20px 0; color:#5CA25E">
                             Contacts</h3>
+                             <h3 class="col-5 col-sm g-font-weight-500 g-font-size-24 text-success justify-content-center"
+                            style="padding:25px 0 20px 0; margin-left: 250px;  ">
+                            Sujets</h3>   
                     </header>
 
                     <?php foreach($GLOBALS['view']['contacts'] as $contact){ ?>
 
-                    <div class="js-custom-scroll-horizontal js-custom-scroll g-height-59_4vh--lg">
+                    
                         <!-- ContactList -->
                         <div
                             class="d-flex d-lg-block g-brd-bottom g-brd-none--lg g-brd-gray-light-v4 g-overflow-x-auto">
@@ -36,26 +40,31 @@ include('includes/header.php') ?>
 
                                     <div class="media-body align-self-center g-hidden-md-down">
                                         <div class="media g-mb-12">
+                                        
                                             <!-- Contact List Item: Name -->
-                                            <h3 class="d-flex align-self-center g-font-size-16 g-font-weight-400 mb-0"
+                                            
+                                            <h3 class="col-4 d-flex align-self-center g-font-size-16 g-font-weight-400 mb-0"
                                                 style="color: #222E44;">
-                                                <?=$contact['nameContact']?></h3>
+                                                <?=$contact['nameContact']?></h3> 
                                             <!-- End Contact List Item: Name -->
 
                                             <!-- Contact List Item: Subject -->
-                                            <h3 class="d-flex align-self-center g-font-size-16 g-font-weight-400 mb-0 ml-auto"
-                                                style="color: #222E44;">
+                                            
+                                            <h3 class="col-4 d-flex align-self-center g-font-size-16 g-font-weight-400 mb-0 ml-auto"
+                                                style="color: #222E44; margin-left: 250px">
                                                 <?=$contact['subjectContact']?></h3>
                                             <!-- End Contact List Item: Subject -->
 
                                             
                                             <!-- Contact List Item: Time -->
+                                            
                                             <em id="date"
-                                                class="d-flex align-self-center align-items-center g-font-style-normal g-color-gray-light-v1 ml-auto"
+                                                class="col-2 d-flex align-self-center align-items-center g-font-style-normal g-color-gray-light-v1 mb-0 ml-auto"
                                                 style="color: #222E44;">
                                                 <i class="hs-admin-time icon-clock g-mr-5"></i>
                                                 <small>14h51</small>
                                             </em>
+                                            
                                             <!-- End Contact List Item: Time -->
                                         </div>
                                         <div class="container d-none  g-mt-28 g-mb-28">
@@ -71,7 +80,7 @@ include('includes/header.php') ?>
                                 </section>
                             </a>
                         </div>
-                    </div>
+                    
                     <?php } ?>
                 </div>
 
@@ -82,10 +91,14 @@ include('includes/header.php') ?>
 
                     <!-- End Contact. Message Area. Header. -->
                     <div class="col-lg-12 w-100 g-brd-right--lg g-brd-gray-light-v7">
-                        <header class="g-brd-bottom g-brd-gray-light-v4 g-px-15 g-px-25--lg justify-content-center">
-                            <h3 id="titleName" class="g-font-weight-500 g-font-size-24 g-color-black justify-content-center"
-                                style="padding:25px 0 20px 0"></h3>
-                        </header>
+                    <header class="row d-inline-flex  g-brd-gray-light-v4 g-px-15 g-px-25--lg">
+                    
+                        <h3 class="col col-sm g-font-weight-500 g-font-size-24 text-success justify-content-center"
+                            style="padding:25px 0 20px 0">
+                            Message</h3>
+                               
+                    </header>
+                        
                         
                         <section class="section">
                             <div class="container">
@@ -94,21 +107,24 @@ include('includes/header.php') ?>
                                 <!-- resultat de la recherche -->
                                 <div class="media-body align-self-center g-hidden-md-down g-mt-28">
                                     <div class="media g-mb-12">
-                                        <h3 id="name"
-                                            class="d-flex align-self-center g-font-size-16 g-font-weight-400 mb-0"
-                                            style="color: #222E44;"></h3>
+                                    <header class="g-brd-gray-light-v4 g-px-15 g-px-25--lg justify-content-center">
+                            <h3 id="titleName" class="g-font-weight-500 g-font-size-24 g-color-black justify-content-center"
+                                style="padding:25px 0 20px 0; margin: auto;color: #49ADEF;"></h3>
+                                
+                        </header>
+                                        
                                         <h3 id="subject"
                                             class="d-flex align-self-center g-font-size-16 g-font-weight-400 mb-0 ml-auto mr-auto"
-                                            style="color: #222E44;"></h3>
+                                            style="color: #49ADEF;"></h3>
                                     </div>
                                     <div class="container g-mt-28 g-mb-28">
                                         <h3 id="message"
                                             class="d-flex align-self-center g-font-size-16 g-font-weight-400 mb-0"
-                                            style="color: #222E44;"></h3>
+                                            style="color: #000000;"></h3>
                                     </div>
                                 </div>
                             </div>
-                            <div class="ml-auto">
+                            <div id="respondTo" class="d-none ml-auto">
                                     <button class="btn btn-link d-flex align-self-top align-items-top u-link-v5 g-color-secondary g-color-secondary--hover p-0 g-ml-15">
                                         <i class="hs-admin-arrow-top-right g-font-size-18 g-line-height-1_4"></i>
                                         <span class="g-hidden-sm-down g-font-weight-300 g-font-size-12 g-font-size-default--md g-ml-4 g-ml-8--md">
@@ -131,13 +147,12 @@ include('includes/header.php') ?>
         function showMessageContact(name, email, subject, content) {
             console.log(name + subject + content);
             document.getElementById("titleName").innerHTML = name;
-            document.getElementById("name").innerHTML = name;
+            
             document.getElementById("subject").innerHTML = subject;
             document.getElementById("message").innerHTML = content;
             document.getElementById("response").innerHTML = name;
-            document.getElementById("email").href = "mailto:"+email;
-
-
+            document.getElementById("email").href = "mailto:"+ email;
+            document.getElementById("respondTo").class.display = "block";
 
         }
     </script>
