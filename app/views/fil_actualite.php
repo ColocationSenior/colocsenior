56f5216c047aa3ab24617128c53973269ecc72bd
Halim AFIFI <afifihalim@hotmail.com>
2019-12-18 12:40:39 +0100

    hr border restyled

--- app/views/fil_actualite.php
+++ app/views/fil_actualite.php
@@ -17,7 +17,7 @@ include('includes/header.php') ?>
                 <h1 class="g-font-weight-500 g-font-size-30 g-color-black mb-0">Les dernières annonces</h1>
             </div>
         </div>
-        <hr class="d-flex  g-my-20" style="border: 2px solid #375D81;border-radius: 2px;">
+        <hr class="d-flex  g-my-20" style="border: 1px solid #375D81;border-radius: 2px;">
         <div class="row">
             <?php foreach($GLOBALS['view']['annonces'] as $annonce){ ?>
             <div class="col-xl-3 col-md-6 g-mb-30">
@@ -90,7 +90,7 @@ include('includes/header.php') ?>
                 <h1 class="g-font-weight-500 g-font-size-30 g-color-black mb-0">Découvrez de nouvelles personnes</h1>
             </div>
         </div>
-        <hr class="d-flex  g-my-20" style="border: 2px solid #375D81;border-radius: 2px;">
+        <hr class="d-flex  g-my-20" style="border: 1px solid #375D81;border-radius: 2px;">
         <div class="row">
         <?php foreach($GLOBALS['view']['users'] as $user){ ?>
             <!-- User Card -->
@@ -132,7 +132,7 @@ include('includes/header.php') ?>
                 <h1 class="g-font-weight-500 g-font-size-30 g-color-black mb-0">Les derniers articles</h1>
             </div>
         </div>
-        <hr class="d-flex  g-my-20" style="border: 2px solid #375D81;border-radius: 2px;">
+        <hr class="d-flex  g-my-20" style="border: 1px solid #375D81;border-radius: 2px;">
         <div class="row">
             <?php foreach($GLOBALS['view']['news'] as $new){ ?>
             <div class="col-xl-3 col-md-6 g-mb-30">
